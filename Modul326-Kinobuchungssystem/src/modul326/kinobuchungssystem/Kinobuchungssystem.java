7d688c46b9461e4bc5b682b8cd3d7dd1568620c5
f.gysel <f.gysel@PC0106.Stiftung.ifa>
2015-07-03 09:57:28 +0200

    gui started

--- Modul326-Kinobuchungssystem/src/modul326/kinobuchungssystem/Kinobuchungssystem.java
+++ Modul326-Kinobuchungssystem/src/modul326/kinobuchungssystem/Kinobuchungssystem.java
@@ -1,4 +1,5 @@
 package modul326.kinobuchungssystem;
+
 import java.util.ArrayList;
 
 /**
@@ -6,174 +7,191 @@ import java.util.ArrayList;
  * @author Timothe
  */
 public class Kinobuchungssystem {
+
     private int naechsteBuchungsnummer;
     private ArrayList<Vorstellung> filme = new ArrayList<Vorstellung>();
     private ArrayList<Reservierung> res = new ArrayList<Reservierung>();
+    public ArrayList<Kinosaal> saele = new ArrayList<Kinosaal>();
 
     public Kinobuchungssystem() {
 
-         
-         Kinosaal saal1 = new Kinosaal(1);
-         Kinosaal saal2 = new Kinosaal(2);
-         
-         Reihe reihe1 = new Reihe(1);
-         Reihe reihe2 = new Reihe(2);
-         Reihe reihe3 = new Reihe(3);
-         Reihe reihe4 = new Reihe(4);
-         
-         Platz platz1 = new Platz(1);
-         Platz platz2 = new Platz(2);
-         Platz platz3 = new Platz(3);
-         Platz platz4 = new Platz(4);
-         Platz platz5 = new Platz(5);
-         Platz platz6 = new Platz(6);
-         Platz platz7 = new Platz(7);
-         Platz platz8 = new Platz(8);
-         
-         
-  
-         saal1.ReiheEinfuegen(reihe1);
-         saal1.ReiheEinfuegen(reihe2);
-         saal2.ReiheEinfuegen(reihe3);
-         saal2.ReiheEinfuegen(reihe4);
-         
-        reihe1.PlatzEinfuegen(platz1);
-        reihe1.PlatzEinfuegen(platz2);
-        reihe2.PlatzEinfuegen(platz3);
-        reihe2.PlatzEinfuegen(platz4);
-        reihe3.PlatzEinfuegen(platz5);
-        reihe3.PlatzEinfuegen(platz6);
-        reihe4.PlatzEinfuegen(platz7);
-        reihe4.PlatzEinfuegen(platz8);
-        
-        
-         filmMachen("derp","derp","derp", saal2);
-         filmMachen("derp","derp","derp", saal1);
-         
-         machReservierung(1, "078 790 23 77",platz1);
-         machReservierung(2, "078 790 23 74",platz5);
-         
-         kaufPlatz(platz2);
-         kaufPlatz(platz3);
-         
-         
-         checkPlätze(platz1,platz2,platz3,platz4,platz5,platz6,platz7,platz8);
+        initStuff();
+
+        filmMachen("Ted", "11.2.2012", "94:11", saele.get(0));
+        filmMachen("Poltergeist der Neue ;)", "5.5.2015", "77:33", saele.get(1));
+
+        machReservierung(1, "078 790 23 77", saele.get(0).reihe.get(0).platz.get(0));
+        machReservierung(2, "078 790 23 74", saele.get(0).reihe.get(2).platz.get(3));
+
+        kaufPlatz(saele.get(0).reihe.get(1).platz.get(2));
+        kaufPlatz(saele.get(0).reihe.get(3).platz.get(0));
+
     }
-    
-    
-    
-    public void checkPlätze(Platz platz1,Platz platz2,Platz platz3,Platz platz4,Platz platz5,Platz platz6,Platz platz7,Platz platz8){
-        if(platz1.istPlatzReserviert()){
-             System.out.println("Platz 1 ist reserviert");
-         }
-        if(platz2.istPlatzReserviert()){
-             System.out.println("Platz 2 ist reserviert");
-         }
-        if(platz3.istPlatzReserviert()){
-             System.out.println("Platz 3 ist reserviert");
-         }
-        if(platz4.istPlatzReserviert()){
-             System.out.println("Platz 4 ist reserviert");
-         }
-        if(platz5.istPlatzReserviert()){
-             System.out.println("Platz 5 ist reserviert");
-         }
-        if(platz6.istPlatzReserviert()){
-             System.out.println("Platz 6 ist reserviert");
-         }
-        if(platz7.istPlatzReserviert()){
-             System.out.println("Platz 7 ist reserviert");
-         }
-        if(platz8.istPlatzReserviert()){
-             System.out.println("Platz 8 ist reserviert");
-         }
-        if(platz1.istPlatzVerkauft()){
-             System.out.println("Platz 1 ist Verkauft");
-         }
-        if(platz2.istPlatzVerkauft()){
-             System.out.println("Platz 2 ist Verkauft");
-         }
-        if(platz3.istPlatzVerkauft()){
-             System.out.println("Platz 3 ist Verkauft");
-         }
-        if(platz4.istPlatzVerkauft()){
-             System.out.println("Platz 4 ist Verkauft");
-         }
-        if(platz5.istPlatzVerkauft()){
-             System.out.println("Platz 5 ist Verkauft");
-         }
-        if(platz6.istPlatzVerkauft()){
-             System.out.println("Platz 6 ist Verkauft");
-         }
-        if(platz7.istPlatzVerkauft()){
-             System.out.println("Platz 7 ist Verkauft");
-         }
-        if(platz8.istPlatzVerkauft()){
-             System.out.println("Platz 8 ist Verkauft");
-         }
-        if(platz1.istPlatzFrei()){
-             System.out.println("Platz 1 ist Frei");
-         }
-        if(platz2.istPlatzFrei()){
-             System.out.println("Platz 2 ist Frei");
-         }
-        if(platz3.istPlatzFrei()){
-             System.out.println("Platz 3 ist Frei");
-         }
-        if(platz4.istPlatzFrei()){
-             System.out.println("Platz 4 ist Frei");
-         }
-        if(platz5.istPlatzFrei()){
-             System.out.println("Platz 5 ist Frei");
-         }
-        if(platz6.istPlatzFrei()){
-             System.out.println("Platz 6 ist Frei");
-         }
-        if(platz7.istPlatzFrei()){
-             System.out.println("Platz 7 ist Frei");
-         }
-        if(platz8.istPlatzFrei()){
-             System.out.println("Platz 8 ist Frei");
-         }
-        
+
+    private void initStuff() {
+        Kinosaal saal1 = new Kinosaal(1);
+        Reihe saal1reihe1 = new Reihe(1);
+        Platz saal1reihe1platz1 = new Platz(1);
+        saal1reihe1.PlatzEinfuegen(saal1reihe1platz1);
+        Platz saal1reihe1platz2 = new Platz(2);
+        saal1reihe1.PlatzEinfuegen(saal1reihe1platz2);
+        Platz saal1reihe1platz3 = new Platz(3);
+        saal1reihe1.PlatzEinfuegen(saal1reihe1platz3);
+        Platz saal1reihe1platz4 = new Platz(4);
+        saal1reihe1.PlatzEinfuegen(saal1reihe1platz4);
+        saal1.ReiheEinfuegen(saal1reihe1);
+
+        Reihe saal1reihe2 = new Reihe(2);
+        Platz saal1reihe2platz1 = new Platz(5);
+        saal1reihe2.PlatzEinfuegen(saal1reihe2platz1);
+        Platz saal1reihe2platz2 = new Platz(6);
+        saal1reihe2.PlatzEinfuegen(saal1reihe2platz2);
+        Platz saal1reihe2platz3 = new Platz(7);
+        saal1reihe2.PlatzEinfuegen(saal1reihe2platz3);
+        Platz saal1reihe2platz4 = new Platz(8);
+        saal1reihe2.PlatzEinfuegen(saal1reihe2platz4);
+        saal1.ReiheEinfuegen(saal1reihe2);
+
+        Reihe saal1reihe3 = new Reihe(3);
+        Platz saal1reihe3platz1 = new Platz(9);
+        saal1reihe3.PlatzEinfuegen(saal1reihe3platz1);
+        Platz saal1reihe3platz2 = new Platz(10);
+        saal1reihe3.PlatzEinfuegen(saal1reihe3platz2);
+        Platz saal1reihe3platz3 = new Platz(11);
+        saal1reihe3.PlatzEinfuegen(saal1reihe3platz3);
+        Platz saal1reihe3platz4 = new Platz(12);
+        saal1reihe3.PlatzEinfuegen(saal1reihe3platz4);
+        saal1.ReiheEinfuegen(saal1reihe3);
+
+        Reihe saal1reihe4 = new Reihe(4);
+        Platz saal1reihe4platz1 = new Platz(13);
+        saal1reihe4.PlatzEinfuegen(saal1reihe4platz1);
+        Platz saal1reihe4platz2 = new Platz(14);
+        saal1reihe4.PlatzEinfuegen(saal1reihe4platz2);
+        Platz saal1reihe4platz3 = new Platz(15);
+        saal1reihe4.PlatzEinfuegen(saal1reihe4platz3);
+        Platz saal1reihe4platz4 = new Platz(16);
+        saal1reihe4.PlatzEinfuegen(saal1reihe4platz4);
+        saal1.ReiheEinfuegen(saal1reihe4);
+        saele.add(saal1);
+
+        Kinosaal saal2 = new Kinosaal(2);
+        Reihe saal2reihe1 = new Reihe(5);
+        Platz saal2reihe1platz1 = new Platz(17);
+        saal2reihe1.PlatzEinfuegen(saal2reihe1platz1);
+        Platz saal2reihe1platz2 = new Platz(18);
+        saal2reihe1.PlatzEinfuegen(saal2reihe1platz2);
+        Platz saal2reihe1platz3 = new Platz(19);
+        saal2reihe1.PlatzEinfuegen(saal2reihe1platz3);
+        Platz saal2reihe1platz4 = new Platz(20);
+        saal2reihe1.PlatzEinfuegen(saal2reihe1platz4);
+        saal2.ReiheEinfuegen(saal2reihe1);
+
+        Reihe saal2reihe2 = new Reihe(6);
+        Platz saal2reihe2platz1 = new Platz(21);
+        saal2reihe2.PlatzEinfuegen(saal2reihe2platz1);
+        Platz saal2reihe2platz2 = new Platz(22);
+        saal2reihe2.PlatzEinfuegen(saal2reihe2platz2);
+        Platz saal2reihe2platz3 = new Platz(23);
+        saal2reihe2.PlatzEinfuegen(saal2reihe2platz3);
+        Platz saal2reihe2platz4 = new Platz(24);
+        saal2reihe2.PlatzEinfuegen(saal2reihe2platz4);
+        saal2.ReiheEinfuegen(saal2reihe2);
+
+        Reihe saal2reihe3 = new Reihe(7);
+        Platz saal2reihe3platz1 = new Platz(25);
+        saal2reihe3.PlatzEinfuegen(saal2reihe3platz1);
+        Platz saal2reihe3platz2 = new Platz(26);
+        saal2reihe3.PlatzEinfuegen(saal2reihe3platz2);
+        Platz saal2reihe3platz3 = new Platz(27);
+        saal2reihe3.PlatzEinfuegen(saal2reihe3platz3);
+        Platz saal2reihe3platz4 = new Platz(28);
+        saal2reihe3.PlatzEinfuegen(saal2reihe3platz4);
+        saal2.ReiheEinfuegen(saal2reihe3);
+
+        Reihe saal2reihe4 = new Reihe(8);
+        Platz saal2reihe4platz1 = new Platz(29);
+        saal2reihe4.PlatzEinfuegen(saal2reihe4platz1);
+        Platz saal2reihe4platz2 = new Platz(30);
+        saal2reihe4.PlatzEinfuegen(saal2reihe4platz2);
+        Platz saal2reihe4platz3 = new Platz(31);
+        saal2reihe4.PlatzEinfuegen(saal2reihe4platz3);
+        Platz saal2reihe4platz4 = new Platz(32);
+        saal2reihe4.PlatzEinfuegen(saal2reihe4platz4);
+        saal2.ReiheEinfuegen(saal2reihe4);
+        saele.add(saal2);
+
     }
-    
-    public void filmMachen(String filmName, String datum, String zeit,Kinosaal saal){
-        Vorstellung film = new Vorstellung(filmName,datum,zeit,saal);
+
+    public void checkPlaetze() {
+        for (Kinosaal saal : saele) {
+            System.out.println("Saal " + saal.nummer + ": ");
+            for (Reihe reihe : saal.reihe) {
+                System.out.print(reihe.reihenNr + ": ");
+                for (Platz platz : reihe.lieferePlätze()) {
+                    if (platz.istPlatzFrei()) {
+                        System.out.print("[ ]");
+                    } else if (platz.istPlatzReserviert()) {
+                        System.out.print("[-]");
+                    } else if (platz.istPlatzVerkauft()) {
+                        System.out.print("[X]");
+                    } else {
+                        System.err.print("[ERROR]");
+                    }
+                }
+                System.out.println("");
+            }
+        System.out.println("");
+        }
+    }
+
+    public void filmMachen(String filmName, String datum, String zeit, Kinosaal saal) {
+        Vorstellung film = new Vorstellung(filmName, datum, zeit, saal);
         filme.add(film);
     }
- 
 
-    public Vorstellung zeigeVorstellung(int a){
-       return filme.get(a);
+    public Vorstellung zeigeVorstellung(int a) {
+        return filme.get(a);
     }
-    
-    public void machReservierung(int reservierungsNr, String telefonNummer, Platz p){
-        Reservierung lel = new Reservierung( reservierungsNr,  telefonNummer, p);
+
+    public void zeigeAlleVorstellung() {
+        for (Vorstellung film : filme) {
+            System.out.print("Film: ");
+            System.out.print(film.filmName + ", ");
+            System.out.print(film.datum + ", ");
+            System.out.print(film.zeit + ", ");
+            System.out.print("Saal " + film.saal.nummer);
+            System.out.println("");
+        }
+    }
+
+    public void machReservierung(int reservierungsNr, String telefonNummer, Platz p) {
+        Reservierung lel = new Reservierung(reservierungsNr, telefonNummer, p);
         res.add(lel);
     }
-    
-    public void ändereReservierung(int reservierungsNr, String telefonNummer, Platz alt,Platz neu,int n){
+
+    public void aendereReservierung(int reservierungsNr, String telefonNummer, Platz alt, Platz neu, int n) {
         alt.freiePlatz();
-        Reservierung lel = new Reservierung( reservierungsNr,  telefonNummer, neu);
-        res.set(n,lel);
-        
+        Reservierung lel = new Reservierung(reservierungsNr, telefonNummer, neu);
+        res.set(n, lel);
+
     }
-    public void storniereReservierung(Platz alt,int n){
+
+    public void storniereReservierung(Platz alt, int n) {
         alt.freiePlatz();
         res.remove(n);
-        
+
     }
-    
+
     public int liefereBuchungNummer() {
         return naechsteBuchungsnummer;
     }
-    
-    public void kaufPlatz(Platz p){
-        if(!p.istPlatzVerkauft()){
+
+    public void kaufPlatz(Platz p) {
+        if (!p.istPlatzVerkauft()) {
             p.verkaufePlatz();
         }
-        
+
     }
- 
+
 }
--- Modul326-Kinobuchungssystem/src/modul326/kinobuchungssystem/Main.java
+++ Modul326-Kinobuchungssystem/src/modul326/kinobuchungssystem/Main.java
@@ -1,6 +1,13 @@
 package modul326.kinobuchungssystem;
+
 public class Main {
-public static void main(String[] args) {
-         Kinobuchungssystem system = new Kinobuchungssystem();    
-    }    
-}  
+
+    public static void main(String[] args) {
+        Kinobuchungssystem system = new Kinobuchungssystem();
+
+        gui gui = new gui(system);
+        //gui.setSize(552, 219);
+        gui.setVisible(true);
+    }
+
+}
